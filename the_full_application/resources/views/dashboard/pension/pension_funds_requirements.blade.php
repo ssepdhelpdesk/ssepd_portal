4a4ead96507a1c0ce829a46c27a53adbf2beb773
ssepdhelpdesk <ssepdhelpdesk@gmail.com>
2025-08-02 12:18:55 +0530

    Provide the beneficiary count, not the amount.

--- the_full_application/resources/views/dashboard/pension/pension_funds_requirements.blade.php
+++ the_full_application/resources/views/dashboard/pension/pension_funds_requirements.blade.php
@@ -60,14 +60,14 @@ Pension || Funds Requiremt
                      @csrf
                      @method('post')
                      <div class="form-body">
-                        <h5 class="card-title">Block/ULB wise fund requirement under MBPY for the pension month August 2025</h5>
+                        <h5 class="card-title">Block/ULB wise fund requirement under MBPY for the pension month August 2025<small>Provide the beneficiary count, not the amount.</small></h5>
                         <hr>
                         <div class="row">
                            <div class="col-md-3">
                               <div class="form-group" id="mbpy_oap_below_80_years_div">
                                  <label class="form-label">MBPOAP (Below 80 Years)<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_oap_below_80_years" name="mbpy_oap_below_80_years" value="{{ old('mbpy_oap_below_80_years') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_oap_below_80_years" name="mbpy_oap_below_80_years" value="{{ old('mbpy_oap_below_80_years') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_oap_below_80_years_error"></div>
                                  @error('mbpy_oap_below_80_years')
                                  <label class="error">{{ $message }}</label>
@@ -78,7 +78,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_oap_above_80_years_div">
                                  <label class="form-label">MBPOAP (Above 80 Years)<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_oap_above_80_years" name="mbpy_oap_above_80_years" value="{{ old('mbpy_oap_above_80_years') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_oap_above_80_years" name="mbpy_oap_above_80_years" value="{{ old('mbpy_oap_above_80_years') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_oap_above_80_years_error"></div>
                                  @error('mbpy_oap_above_80_years')
                                  <label class="error">{{ $message }}</label>
@@ -89,7 +89,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_wp_div">
                                  <label class="form-label">MBPWP<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_wp" name="mbpy_wp" value="{{ old('mbpy_wp') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_wp" name="mbpy_wp" value="{{ old('mbpy_wp') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_wp_error"></div>
                                  @error('mbpy_wp')
                                  <label class="error">{{ $message }}</label>
@@ -100,7 +100,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_dp_div">
                                  <label class="form-label">MBPDP<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_dp" name="mbpy_dp" value="{{ old('mbpy_dp') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_dp" name="mbpy_dp" value="{{ old('mbpy_dp') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_dp_error"></div>
                                  @error('mbpy_dp')
                                  <label class="error">{{ $message }}</label>
@@ -111,7 +111,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_sdp_below_80_percent_div">
                                  <label class="form-label">MBPSDP (Below 80%)<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_sdp_below_80_percent" name="mbpy_sdp_below_80_percent" value="{{ old('mbpy_sdp_below_80_percent') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_sdp_below_80_percent" name="mbpy_sdp_below_80_percent" value="{{ old('mbpy_sdp_below_80_percent') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_sdp_below_80_percent_error"></div>
                                  @error('mbpy_sdp_below_80_percent')
                                  <label class="error">{{ $message }}</label>
@@ -122,7 +122,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_sdp_above_80_percent_div">
                                  <label class="form-label">MBPSDP (Above 80%)<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_sdp_above_80_percent" name="mbpy_sdp_above_80_percent" value="{{ old('mbpy_sdp_above_80_percent') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_sdp_above_80_percent" name="mbpy_sdp_above_80_percent" value="{{ old('mbpy_sdp_above_80_percent') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_sdp_above_80_percent_error"></div>
                                  @error('mbpy_sdp_above_80_percent')
                                  <label class="error">{{ $message }}</label>
@@ -133,7 +133,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_sdoap_div">
                                  <label class="form-label">MBPSDOAP<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_sdoap" name="mbpy_sdoap" value="{{ old('mbpy_sdoap') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_sdoap" name="mbpy_sdoap" value="{{ old('mbpy_sdoap') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_sdoap_error"></div>
                                  @error('mbpy_sdoap')
                                  <label class="error">{{ $message }}</label>
@@ -144,7 +144,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_clp_div">
                                  <label class="form-label">MBPCLP<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_clp" name="mbpy_clp" value="{{ old('mbpy_clp') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_clp" name="mbpy_clp" value="{{ old('mbpy_clp') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_clp_error"></div>
                                  @error('mbpy_clp')
                                  <label class="error">{{ $message }}</label>
@@ -155,7 +155,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_wp_aids_div">
                                  <label class="form-label">MBPWP (Due to Aids)<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_wp_aids" name="mbpy_wp_aids" value="{{ old('mbpy_wp_aids') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_wp_aids" name="mbpy_wp_aids" value="{{ old('mbpy_wp_aids') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_wp_aids_error"></div>
                                  @error('mbpy_wp_aids')
                                  <label class="error">{{ $message }}</label>
@@ -166,7 +166,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_dp_aids_div">
                                  <label class="form-label">MBPDP (Due to Aids)<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_dp_aids" name="mbpy_dp_aids" value="{{ old('mbpy_dp_aids') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_dp_aids" name="mbpy_dp_aids" value="{{ old('mbpy_dp_aids') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_dp_aids_error"></div>
                                  @error('mbpy_dp_aids')
                                  <label class="error">{{ $message }}</label>
@@ -177,7 +177,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_unmarried_women_div">
                                  <label class="form-label">MBPUMW<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_unmarried_women" name="mbpy_unmarried_women" value="{{ old('mbpy_unmarried_women') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_unmarried_women" name="mbpy_unmarried_women" value="{{ old('mbpy_unmarried_women') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_unmarried_women_error"></div>
                                  @error('mbpy_unmarried_women')
                                  <label class="error">{{ $message }}</label>
@@ -188,7 +188,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_orphan_due_to_covide_div">
                                  <label class="form-label">Orphan due to Covid<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_orphan_due_to_covide" name="mbpy_orphan_due_to_covide" value="{{ old('mbpy_orphan_due_to_covide') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_orphan_due_to_covide" name="mbpy_orphan_due_to_covide" value="{{ old('mbpy_orphan_due_to_covide') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_orphan_due_to_covide_error"></div>
                                  @error('mbpy_orphan_due_to_covide')
                                  <label class="error">{{ $message }}</label>
@@ -199,7 +199,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_widow_due_to_covid_div">
                                  <label class="form-label">Widow due to Covid<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_widow_due_to_covid" name="mbpy_widow_due_to_covid" value="{{ old('mbpy_widow_due_to_covid') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_widow_due_to_covid" name="mbpy_widow_due_to_covid" value="{{ old('mbpy_widow_due_to_covid') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_widow_due_to_covid_error"></div>
                                  @error('mbpy_widow_due_to_covid')
                                  <label class="error">{{ $message }}</label>
@@ -210,7 +210,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_divorce_or_destitute_div">
                                  <label class="form-label">Divorcee or Destitute<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_divorce_or_destitute" name="mbpy_divorce_or_destitute" value="{{ old('mbpy_divorce_or_destitute') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_divorce_or_destitute" name="mbpy_divorce_or_destitute" value="{{ old('mbpy_divorce_or_destitute') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_divorce_or_destitute_error"></div>
                                  @error('mbpy_divorce_or_destitute')
                                  <label class="error">{{ $message }}</label>
@@ -221,7 +221,7 @@ Pension || Funds Requiremt
                               <div class="form-group" id="mbpy_transgender_div">
                                  <label class="form-label">Transgender<span class="itsrequired"> *</span></label>
                                  <input 
-                                 type="number" id="mbpy_transgender" name="mbpy_transgender" value="{{ old('mbpy_transgender') }}" class="form-control" placeholder="Enter count" min="0" step="1">
+                                 type="number" id="mbpy_transgender" name="mbpy_transgender" value="{{ old('mbpy_transgender') }}" class="form-control" placeholder="Enter beneficiary count" min="0" step="1">
                                  <div id="mbpy_transgender_error"></div>
                                  @error('mbpy_transgender')
                                  <label class="error">{{ $message }}</label>
