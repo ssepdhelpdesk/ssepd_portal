6bc0afc6e5c9f92266c3daaea7e3792d80b1f599
ssepdhelpdesk <ssepdhelpdesk@gmail.com>
2026-01-09 14:17:41 +0530

    Aadhaar Verification Work Started in EP Pensioners

--- the_full_application/routes/console.php
+++ the_full_application/routes/console.php
@@ -9,13 +9,13 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('oldage:aadhar-verify 200')
-    ->everyFourMinutes()
+    ->everyFiveMinutes()
     ->withoutOverlapping()
     ->runInBackground()
     ->onOneServer();
 
 Schedule::command('disability:aadhar-verify 200')
-    ->everySixMinutes()
+    ->everyFiveMinutes()
     ->withoutOverlapping()
     ->runInBackground()
     ->onOneServer();
