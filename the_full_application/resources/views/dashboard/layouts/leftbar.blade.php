bf293aa8ca32b70a6f6e6d81e57dd7649d4400c3
ssepdhelpdesk <ssepdhelpdesk@gmail.com>
2026-06-22 16:58:02 +0530

    Remove runtime generated PIA files from repository

--- the_full_application/resources/views/dashboard/layouts/leftbar.blade.php
+++ the_full_application/resources/views/dashboard/layouts/leftbar.blade.php
@@ -181,7 +181,7 @@
                   <li>
                      <a class="has-arrow" href="javascript:void(0)" aria-expanded="false">PIA/NGO Institutes</a>
                      <ul aria-expanded="false" class="collapse">
-                        @can('Institute-list')
+                        @can('Institute-create')
                         <li><a href="{{route('admin.piainstitutes.pia_institute_basic_details')}}">Add Beneficiary Details</a></li>
                         @endcan
                         @can('Institute-list')
