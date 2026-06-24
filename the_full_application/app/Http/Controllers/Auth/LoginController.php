18c43bcd784f0cf5c06c65e13eec3ce41b12823b
ssepdhelpdesk <ssepdhelpdesk@gmail.com>
2026-06-24 11:35:33 +0530

    The Logout Functionality for Normal user & Insttute User

--- the_full_application/app/Http/Controllers/Auth/LoginController.php
+++ the_full_application/app/Http/Controllers/Auth/LoginController.php
@@ -83,13 +83,45 @@ class LoginController extends Controller
         $user = auth()->user();
         $redirectUrl = '/'; // Default redirect
         
+        \Log::info('Logout method called', [
+            'user' => $user ? [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ] : null,
+        ]);
+
         if ($user) {
             $userRoles = $user->roles->pluck('id')->toArray();
             $instituteRoles = [16, 17, 22, 23, 24];
             $hasInstituteRole = !empty(array_intersect($userRoles, $instituteRoles));
             
+            \Log::info('Checking user roles for logout redirect', [
+                'user_roles' => $userRoles,
+                'institute_roles' => $instituteRoles,
+                'has_institute_role' => $hasInstituteRole,
+            ]);
+
             if ($hasInstituteRole) {
                 $redirectUrl = '/pia_institute_login';
+            }
+        }
+
+        // Handle login history logout update
+        $loginHistoryId = $request->session()->get('login_history_id');
+        if ($loginHistoryId) {
+            $loginHistory = \App\Models\LoginHistory::find($loginHistoryId);
+            if ($loginHistory && $loginHistory->login_time) {
+                $loginHistory->logout_date_time = \Carbon\Carbon::now('Asia/Kolkata');
+                $loginHistory->logout_time = \Carbon\Carbon::now('Asia/Kolkata')->format('H:i:s');
+                $loginTime = \Carbon\Carbon::createFromFormat('H:i:s', $loginHistory->login_time);
+                $logoutTime = \Carbon\Carbon::createFromFormat('H:i:s', $loginHistory->logout_time);
+                $sessionDuration = $logoutTime->diff($loginTime)->format('%H:%I:%S');
+                $loginHistory->session_duration = $sessionDuration;                                
+                $loginHistory->save();
+                $request->session()->forget('login_history_id');
+            } else {
+                \Log::warning("Login history record with ID $loginHistoryId not found during LoginController logout.");
             }
         }
 
@@ -98,6 +130,10 @@ class LoginController extends Controller
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
+
+        \Log::info('Logout completed', [
+            'redirectUrl' => $redirectUrl,
+        ]);
 
         if ($response = $this->loggedOut($request)) {
             return $response;
